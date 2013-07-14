2fc43c9c01a71e8838d8fcdbf3c1068bb80b6ae5
adamthedeveloper <adam@cakestyle.com>
2013-07-14 03:53:22 -0700

    Added all project files initially

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unsolved::Application.config.secret_key_base = '1260b4219da56bb401398f846053ae5f0210a1534d87b3aac319e29e53fa88c27a62fbe2c58bee166f7c0d54562b356cfdf5319656bcb0c6d503210905129405'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unsolved::Application.config.session_store :cookie_store, key: '_unsolved_session'
